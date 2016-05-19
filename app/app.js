93cb56de767dc273edd8183a95db0d83d90b7272
utleroy <bryanhankins@me.com>
2016-05-18 21:18:46 -0500

    add dynamic nav

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@
 
 var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", function($scope) {
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+})
+
 app.controller("TodoCtrl", function($scope) {
 	$scope.welcome = "hello";
 })
