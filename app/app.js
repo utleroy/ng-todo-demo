c6b46e463de33c576ab22214f98ddbf830c047bf
utleroy <bryanhankins@me.com>
2016-05-25 18:44:20 -0500

    add firebase constant

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
-var app = angular.module("TodoApp", ["ngRoute"]);
+var app = angular.module("TodoApp", ["ngRoute"])
+	.constant("firebaseURL","https://leroy-todo.firebaseio.com/");
 
 app.config(function($routeProvider) {
 	$routeProvider.
--- app/factories/itemFactory.js
+++ app/factories/itemFactory.js
@@ -1,10 +1,11 @@
 "use strict";
-app.factory("itemStorage", function($q, $http){
+
+app.factory("itemStorage", function($q, $http, firebaseURL){
 	
 	var getItemList = function(){
 		let items = [];
 		return $q(function(resolve, reject){
-			$http.get("https://leroy-todo.firebaseio.com/items.json")
+			$http.get(firebaseURL + "items.json")
 			.success(function(itemObject){
 				var itemCollection = itemObject;
 				Object.keys(itemCollection).forEach(function(key){
@@ -22,7 +23,7 @@ app.factory("itemStorage", function($q, $http){
 	var deleteItem = function(itemId){
 		return $q(function(resolve, reject){
 			$http
-			.delete(`https://leroy-todo.firebaseio.com/items/${itemId}.json`)
+			.delete(firebaseURL + "items/" + itemId + ".json")
 			.success(function(objectFromFirebase){
 				resolve(objectFromFirebase);
 			});
@@ -32,7 +33,7 @@ app.factory("itemStorage", function($q, $http){
 	var postNewItem = function(newItem){
 		return $q(function(resolve, reject) {
 			$http.post(
-				"https://leroy-todo.firebaseio.com/items.json",
+				firebaseURL + "items.json",
 				JSON.stringify({
 					assignedTo: newItem.assignedTo,
 					dependencies: newItem.dependencies,
