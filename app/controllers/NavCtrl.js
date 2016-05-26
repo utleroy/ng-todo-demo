80846a52c65ba896b5de2231780d262b5b3df69d
utleroy <bryanhankins@me.com>
2016-05-25 19:21:49 -0500

    add nav-bar page

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,16 +1,3 @@
 app.controller("NavCtrl", function($scope) {
-	$scope.navItems = [
-	{
-	 name: "Logout",
-	 url: "#/logout"
-	},
-	{
-	 name: "All Items",
-	 url: "#/items/list"
-	}, 
-	{
-	 name: "New Item",
-	 url: "#/items/new"
-	}
-	];
+	
 });
